5682d38da55618c2eba1a597b03860ccae8ab8a1
Connor Rust <rust485@gmail.com>
2019-05-17 18:15:54 -0500

    move samples to samples.js

---
+++ samples.js
@@ -0,0 +1,76 @@
+function getNodeSample1()
+{
+	let m1 = new MapNode(10, 10);
+	let m2 = new MapNode(110, 110);
+	let m3 = new MapNode(10, 110);
+	let m4 = new MapNode(110, 10);
+
+	return [m1, m2, m3, m4];
+}
+
+function getNodeSample2()
+{
+	let m1 = new MapNode(10, 10);
+	let m2 = new MapNode(110, 110);
+	let m3 = new MapNode(10, 110);
+	let m4 = new MapNode(110, 10);
+
+	let m5 = new MapNode(45, 45);
+	let m6 = new MapNode(75, 75);
+	let m7 = new MapNode(45, 75);
+	let m8 = new MapNode(75, 45);
+
+	return [m1, m2, m3, m4, m5, m6, m7, m8];
+}
+
+function getNodeSample3()
+{
+	let m1 = new MapNode(10, 10);
+	let m2 = new MapNode(30, 10);
+	let m3 = new MapNode(100, 100);
+	let m4 = new MapNode(100, 200);
+
+	return [m1, m2, m3, m4];
+}
+
+function getNodeSample4()
+{
+	let m1 = new MapNode(10, 10);
+	let m2 = new MapNode(100, 100);
+	let m3 = new MapNode(10, 100);
+	let m4 = new MapNode(100, 10);
+	let m5 = new MapNode(35, 75);
+
+	return [m1, m2, m3, m4, m5];
+}
+
+function getNodeSample5()
+{
+	let m1 = new MapNode(10, 10);
+	let m2 = new MapNode(10, 100);
+	let m3 = new MapNode(55, 55);
+	let m4 = new MapNode(200, 55);
+
+	return [m1, m2, m3, m4];
+}
+
+function getNodeSample6()
+{
+	let m1 = new MapNode(20, 20);
+	let m2 = new MapNode(100, 25);
+	let m3 = new MapNode(80, 15);
+	let m4 = new MapNode(200, 100);
+
+	return [m1, m2, m3, m4];
+}
+
+function getNodeSample7()
+{
+	let m1 = new MapNode(50, 100);
+	let m2 = new MapNode(150, 100);
+	let m3 = new MapNode(100, 80);
+	let m4 = new MapNode(100, 50);
+	let m5 = new MapNode(100, 20);
+
+	return [m1, m2, m3, m4, m5];
+}
--- sketch.js
+++ sketch.js
@@ -4,6 +4,10 @@ let fps = .25;
 let renderN = true;
 let edges = [];
 
+/**
+ * Initialize the canvas and start the algorithm
+ * @method setup
+ */
 function setup()
 {
 	createCanvas(800, 800);
@@ -17,17 +21,21 @@ function setup()
 	edges.forEach((e) => renderEdge(e));
 }
 
+/**
+ * On mouse pressed hook
+ * @method mousePressed
+ */
 function mousePressed()
 {
+	// create a new node at the current mouse position
 	nodes.push(new MapNode(mouseX, mouseY));
-	edges = [];
 
-	console.log(nodes);
-
-	delaunay(nodes);
+	// recalculate delaunay
+	edges = delaunay(nodes);
 	background(000);
-	nodes.forEach((n) => n.render());
 
+
+	nodes.forEach((n) => n.render());
 	edges.forEach((e) => renderEdge(e));
 }
 
@@ -53,83 +61,6 @@ function renderNeighbors()
 			renderEdge(e);
 		})
 	});
-}
-
-function getNodeSample1()
-{
-	let m1 = new MapNode(10, 10);
-	let m2 = new MapNode(110, 110);
-	let m3 = new MapNode(10, 110);
-	let m4 = new MapNode(110, 10);
-
-	return [m1, m2, m3, m4];
-}
-
-function getNodeSample2()
-{
-	let m1 = new MapNode(10, 10);
-	let m2 = new MapNode(110, 110);
-	let m3 = new MapNode(10, 110);
-	let m4 = new MapNode(110, 10);
-
-	let m5 = new MapNode(45, 45);
-	let m6 = new MapNode(75, 75);
-	let m7 = new MapNode(45, 75);
-	let m8 = new MapNode(75, 45);
-
-	return [m1, m2, m3, m4, m5, m6, m7, m8];
-}
-
-function getNodeSample3()
-{
-	let m1 = new MapNode(10, 10);
-	let m2 = new MapNode(30, 10);
-	let m3 = new MapNode(100, 100);
-	let m4 = new MapNode(100, 200);
-
-	return [m1, m2, m3, m4];
-}
-
-function getNodeSample4()
-{
-	let m1 = new MapNode(10, 10);
-	let m2 = new MapNode(100, 100);
-	let m3 = new MapNode(10, 100);
-	let m4 = new MapNode(100, 10);
-	let m5 = new MapNode(35, 75);
-
-	return [m1, m2, m3, m4, m5];
-}
-
-function getNodeSample5()
-{
-	let m1 = new MapNode(10, 10);
-	let m2 = new MapNode(10, 100);
-	let m3 = new MapNode(55, 55);
-	let m4 = new MapNode(200, 55);
-
-	return [m1, m2, m3, m4];
-}
-
-function getNodeSample6()
-{
-	let m1 = new MapNode(20, 20);
-	let m2 = new MapNode(100, 25);
-	let m3 = new MapNode(80, 15);
-	let m4 = new MapNode(200, 100);
-
-	return [m1, m2, m3, m4];
-}
-
-function getNodeSample7()
-{
-	let m1 = new MapNode(50, 100);
-	let m2 = new MapNode(150, 100);
-	let m3 = new MapNode(100, 80);
-	let m4 = new MapNode(100, 50);
-	let m5 = new MapNode(100, 20);
-
-	return [m1, m2, m3, m4, m5];
 }
 
 function getRandomNodes()
